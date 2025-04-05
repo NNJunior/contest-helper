9c54a271d6303a4cd1bcb0f193d4011d932fbef8
NNJunior <27612785+NNJunior@users.noreply.github.com>
2025-04-05 13:00:38 +0300

    Changed argument order

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ init_parser = subparsers.add_parser(commands.INIT, description="inititalizes new
 init_parser.add_argument('name', help="name of the new environment")
 
 run_parser = subparsers.add_parser(commands.RUN, description="executes tests")
-
 run_parser.add_argument('-8', '--inf', help="Generate and run tests forever", action='store_true')
 run_parser.add_argument('-t', '--timeout', help="timeout for tests", default=1)
-run_parser.add_argument('-T', '--tests', help="Tests to run (default all). Ignored when --inf or -8 is set", action='extend', nargs='+')
-
+run_parser.add_argument('-i', '--input', help="Displays input", action='store_true')
+run_parser.add_argument('-o', '--output', help="Displays output", action='store_true')
+run_parser.add_argument('-e', '--errors', help="Displays errors", action='store_true')
+run_parser.add_argument('-ce', '--checker-errors', help="Displays checker output", action='store_true')
+run_parser.add_argument('-co', '--checker-output', help="Displays checker errors", action='store_true')
+run_parser.add_argument('--tests', help="Tests to run (default all). Ignored when --inf or -8 is set", action='extend', nargs='+')
 print(parser.parse_args(args))
