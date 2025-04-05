926540ae3c44ad24b6f55c0443bff448a498736d
NNJunior <27612785+NNJunior@users.noreply.github.com>
2025-04-05 17:03:03 +0300

    Added logging functionality

--- src/color.py
+++ src/color.py
@@ -12,26 +12,26 @@ def mark_warning():
 def clear_marks():
     print('\033[0m', end='')
 
-def print_error(error: str, exit_code=1):
+def print_error(*error, exit_code=1):
     mark_error()
     print('Error:', end=' ')
     clear_marks()
-    print(error)
+    print(*error)
     if exit_code is not None:
         sys.exit(exit_code)
 
-def print_warning(warning: str, exit_code=None):
+def print_warning(*warning, exit_code=None):
     mark_warning()
     print('Warning:', end=' ')
     clear_marks()
-    print(warning)
+    print(*warning)
     if exit_code is not None:
         sys.exit(exit_code)
 
-def print_info(info: str, exit_code=None):
+def print_info(*info, exit_code=None):
     mark_info()
     print('Info:', end=' ')
     clear_marks()
-    print(info)
+    print(*info)
     if exit_code is not None:
         sys.exit(exit_code)
